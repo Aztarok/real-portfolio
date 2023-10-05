31109c13d08586829452d96c5d42887c78883066
Aztarok <aztarok2994@gmail.com>
2023-10-05 12:55:05 -0400

    Added scrolling into view with useref to the site

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,37 @@ export default function Home() {
                 <span className="text-[4vw]">DANIEL BOYKOV</span>
                 <span>FRONTEND DEVELOPER & SOFTWARE ENGINEER</span>
             </div>
-            <div className="flex bg-transparent 0 items-center justify-center flex-col w-full h-[20vw] " id="home">
-                <span className="">DANIEL BOYKOV</span>
-                <span>FRONTEND DEVELOPER & SOFTWARE ENGINEER</span>
+            <div className="flex bg-transparent 0 items-center justify-center flex-col w-full h-[50vw] " id="about">
+                <span className="text-[5vw]">About</span>
+                <div className="flex border-[1.5px] w-[75vw] justify-between h-[50%] bg-blue-500">
+                    <div className="flex justify-center w-[33%] bg-slate-600">Software Development</div>
+                    <div className="flex justify-center w-[33%] bg-slate-600">Frontend</div>
+                    <div className="flex justify-center w-[33%] bg-slate-600">Future</div>
+                </div>
+            </div>
+            <div className="flex bg-transparent 0 items-center justify-center flex-col w-full h-[50vw] " id="tech">
+                <span className="text-[5vw]">About</span>
+                <div className="flex border-[1.5px] w-[75vw] justify-between h-[50%] bg-blue-500">
+                    <div className="flex justify-center w-[33%] bg-slate-600">Software Development</div>
+                    <div className="flex justify-center w-[33%] bg-slate-600">Frontend</div>
+                    <div className="flex justify-center w-[33%] bg-slate-600">Future</div>
+                </div>
+            </div>
+            <div className="flex bg-transparent 0 items-center justify-center flex-col w-full h-[50vw] " id="projects">
+                <span className="text-[5vw]">About</span>
+                <div className="flex border-[1.5px] w-[75vw] justify-between h-[50%] bg-blue-500">
+                    <div className="flex justify-center w-[33%] bg-slate-600">Software Development</div>
+                    <div className="flex justify-center w-[33%] bg-slate-600">Frontend</div>
+                    <div className="flex justify-center w-[33%] bg-slate-600">Future</div>
+                </div>
+            </div>
+            <div className="flex bg-transparent 0 items-center justify-center flex-col w-full h-[50vw] " id="contact">
+                <span className="text-[5vw]">About</span>
+                <div className="flex border-[1.5px] w-[75vw] justify-between h-[50%] bg-blue-500">
+                    <div className="flex justify-center w-[33%] bg-slate-600">Software Development</div>
+                    <div className="flex justify-center w-[33%] bg-slate-600">Frontend</div>
+                    <div className="flex justify-center w-[33%] bg-slate-600">Future</div>
+                </div>
             </div>
         </div>
     );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
+"use client";
+import React, { useRef, useEffect } from 'react';
 
 function Header() {
-  return (
-    <div className="flex items-center justify-center fixed top-0 left-0 w-full h-[5vw] bg-transparent gap-[3vw]">
-      <span className="absolute left-[5vw] text-[1.5vw]">Daniel Boykov</span>
-      <button className="duration-300 hover:bg-slate-900 rounded-lg border-[1.5px] border-slate-700 hover:border-white px-[2vw] py-[0.5vw]">Home</button>
-      <button className="duration-300 hover:bg-slate-900 rounded-lg border-[1.5px] border-slate-700 hover:border-white px-[2vw] py-[0.5vw]">About</button>
-      <button className="duration-300 hover:bg-slate-900 rounded-lg border-[1.5px] border-slate-700 hover:border-white px-[2vw] py-[0.5vw]">Technologies</button>
-      <button className="duration-300 hover:bg-slate-900 rounded-lg border-[1.5px] border-slate-700 hover:border-white px-[2vw] py-[0.5vw]">Contact</button>
-    </div>
-  );
+    const sectionRef = useRef({});
+
+    const updateSectionRefs = () => {
+        // Add references to the sections by their IDs
+        ["home", "about", "tech", "projects", "contact"].forEach((sectionId) => {
+          sectionRef.current[sectionId] = document.getElementById(sectionId);
+        });
+      };
+    
+      // Call the function to update sectionRef.current on component mount
+      useEffect(() => {
+        updateSectionRefs();
+      }, []);
+
+    const scrollToSection = (sectionId) => {
+
+        if (sectionRef.current[sectionId]) {
+            sectionRef.current[sectionId].scrollIntoView({behavior: "smooth"});
+        }
+    }
+        
+    return (
+        <div className="flex items-center justify-center fixed top-0 left-0 w-full h-[5vw] bg-slate-950 gap-[3vw]">
+        <span className="absolute left-[5vw] text-[1.5vw]">Daniel Boykov</span>
+        <button onClick={() => scrollToSection("home")} className="duration-300 hover:bg-slate-900 rounded-lg border-[1.5px] border-slate-700 hover:border-white px-[2vw] py-[0.5vw]">Home</button>
+        <button onClick={() => scrollToSection("about")} className="duration-300 hover:bg-slate-900 rounded-lg border-[1.5px] border-slate-700 hover:border-white px-[2vw] py-[0.5vw]">About</button>
+        <button onClick={() => scrollToSection("tech")} className="duration-300 hover:bg-slate-900 rounded-lg border-[1.5px] border-slate-700 hover:border-white px-[2vw] py-[0.5vw]">Technologies</button>
+        <button onClick={() => scrollToSection("projects")} className="duration-300 hover:bg-slate-900 rounded-lg border-[1.5px] border-slate-700 hover:border-white px-[2vw] py-[0.5vw]">Projects</button>
+        <button onClick={() => scrollToSection("contact")} className="duration-300 hover:bg-slate-900 rounded-lg border-[1.5px] border-slate-700 hover:border-white px-[2vw] py-[0.5vw]">Contact</button>
+        </div>
+    );
 }
 
 export default Header;
