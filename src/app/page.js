1b46f70126b9d8432ea83330224fb70b31140673
Aztarok <aztarok2994@gmail.com>
2023-10-11 17:47:46 -0400

    changed project section a bit

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Projects from "@/components/Projects";
 
 export default function Home() {
     const currentColor = 1;
@@ -45,25 +46,7 @@ export default function Home() {
                     <div className="flex justify-center w-[33%] bg-slate-600">Future</div>
                 </div>
             </div>
-            <div className="flex bg-red-400 0 gap-[2vw] items-center justify-center flex-col w-full h-[100vw] " id="projects">
-                <span className="text-[5vw]">Projects</span>
-                <div className="flex border-[.5vw] w-[99vw] justify-between h-[35%] bg-blue-500">
-                    <div className="flex justify-center flex-wrap overflow-hidden w-[33%] bg-slate-600">
-                        <div className="h-[75%] bg-green-500 w-full">
-                            <img src="./next.svg" alt="project 1 image" />
-                        </div>
-                        <div className="h-[15%] bg-slate-700 w-full">hi</div>
-                        <div className="h-[15%] bg-slate-500 w-full">hi</div>
-                    </div>
-                    <div className="flex justify-center w-[33%] bg-slate-600">Project site</div>
-                    <div className="flex justify-center w-[33%] h-[15%] bg-slate-600">Github</div>
-                </div>
-                <div className="flex border-[.5vw] w-[99vw] justify-between h-[35%] bg-blue-500">
-                    <div className="flex justify-center w-[33%] bg-slate-600">Software Development</div>
-                    <div className="flex justify-center w-[33%] bg-slate-600">Frontend</div>
-                    <div className="flex justify-center w-[33%] bg-slate-600">Future</div>
-                </div>
-            </div>
+            <Projects />
             <div className="flex bg-zinc-900 0 items-center justify-center flex-col w-full h-[50vw] " id="contact">
                 <span className="text-[5vw]">Contact</span>
                 <div className="flex border-[1.5px] w-[75vw] justify-between h-[50%] bg-blue-500">
---
+++ src/components/Projects.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className="flex bg-slate-400 0 gap-[2vw] items-center justify-center flex-col w-full h-[100vw] " id="projects">
+        <span className="text-[5vw]">Projects</span>
+        <div className="flex border-[.5vw] w-[99vw] justify-between h-[35%] bg-blue-500">
+            <div className="flex justify-center flex-wrap overflow-hidden w-[33%] bg-slate-600">
+                <div className="h-[75%] bg-green-500 w-full">
+                    <img src="./next.svg" alt="project 1 image" />
+                </div>
+                <div className="h-[15%] bg-slate-700 w-full">hi</div>
+                <div className="h-[15%] bg-slate-500 w-full">hi</div>
+            </div>
+
+            <div className="flex justify-center flex-wrap overflow-hidden w-[33%] bg-slate-600">
+                <div className="h-[75%] bg-green-500 w-full">
+                    <img src="./next.svg" alt="project 1 image" />
+                </div>
+                <div className="h-[15%] bg-slate-700 w-full">hi</div>
+                <div className="h-[15%] bg-slate-500 w-full">hi</div>
+            </div>
+            
+            <div className="flex justify-center flex-wrap overflow-hidden w-[33%] bg-slate-600">
+                <div className="h-[75%] bg-green-500 w-full">
+                    <img src="./next.svg" alt="project 1 image" />
+                </div>
+                <div className="h-[15%] bg-slate-700 w-full">hi</div>
+                <div className="h-[15%] bg-slate-500 w-full">hi</div>
+            </div>
+        </div>
+        <div className="flex border-[.5vw] w-[99vw] justify-between h-[35%] bg-blue-500">
+        <div className="flex justify-center flex-wrap overflow-hidden w-[33%] bg-slate-600">
+                <div className="h-[75%] bg-green-500 w-full">
+                    <img src="./next.svg" alt="project 1 image" />
+                </div>
+                <div className="h-[15%] bg-slate-700 w-full">hi</div>
+                <div className="h-[15%] bg-slate-500 w-full">hi</div>
+            </div>
+            
+            <div className="flex justify-center flex-wrap overflow-hidden w-[33%] bg-slate-600">
+                <div className="h-[75%] bg-green-500 w-full">
+                    <img src="./next.svg" alt="project 1 image" />
+                </div>
+                <div className="h-[15%] bg-slate-700 w-full">hi</div>
+                <div className="h-[15%] bg-slate-500 w-full">hi</div>
+            </div>
+            
+            <div className="flex justify-center flex-wrap overflow-hidden w-[33%] bg-slate-600">
+                <div className="h-[75%] bg-green-500 w-full">
+                    <img src="./next.svg" alt="project 1 image" />
+                </div>
+                <div className="h-[15%] bg-slate-700 w-full">hi</div>
+                <div className="h-[15%] bg-slate-500 w-full">hi</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Projects
